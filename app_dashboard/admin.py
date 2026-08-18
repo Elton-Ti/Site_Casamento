fb7f86c61f5033620abc414d6e3beae8ad2fc441
Elton Lopes Pereira <87839129+Elton-Ti@users.noreply.github.com>
2026-08-17 23:16:52 -0300

    fix: organizar site de casamento na raiz do repositório

--- app_dashboard/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Produto
-
-@admin.register(Produto)
-class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ["id_produto", "nome", "categoria", "marca", "preco", "estoque", "ativo"]
-    list_filter = ["categoria", "marca", "ativo"]
-    search_fields = ["nome", "marca"]
